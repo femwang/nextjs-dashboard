4dbfd6fe495c69c187eada0a0b4f9eb2fe312230
Felix Wangondu <femwangdallas@gmail.com>
2024-05-10 00:35:34 -0500

    page1

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,13 +1,32 @@
-'use client';
+// 'use client';
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-// export default function Search() {
-//   const searchParams = useSearchParams();
-//   const pathname = usePathname();
-//   const { replace } = useRouter();
+// import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+// import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+// // export default function Search() {
+// //   const searchParams = useSearchParams();
+// //   const pathname = usePathname();
+// //   const { replace } = useRouter();
+// //   function handleSearch(term: string) {
+// //     // console.log(term);
+// //     const params = new URLSearchParams(searchParams);
+// //     if (term) {
+// //       params.set('query', term);
+// //     } else {
+// //       params.delete('query');
+// //     }
+// //     replace(`${pathname}?${params.toString()}`);
+// //   }
+// // export default function Search({ placeholder }: { placeholder: string }) {
+//   export default function Search() {
+//     const searchParams = useSearchParams();
+//     const pathname = usePathname();
+//     const { replace } = useRouter();
+
+//   // function handleSearch(term: string) {
+//   //   console.log(term);
+//   // }
 //   function handleSearch(term: string) {
-//     // console.log(term);
+//     console.log(`Searching... ${term}`);
 //     const params = new URLSearchParams(searchParams);
 //     if (term) {
 //       params.set('query', term);
@@ -16,10 +35,58 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 //     }
 //     replace(`${pathname}?${params.toString()}`);
 //   }
+// }
+
+//   return (
+//     <div className="relative flex flex-1 flex-shrink-0">
+//       <label htmlFor="search" className="sr-only">
+//         Search
+//       </label>
+//       <input
+//         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+//         placeholder={placeholder}
+//         onChange={(e) => {
+//         handleSearch(e.target.value);
+//         }}
+//         defaultValue={searchParams.get('query')?.toString()}
+//       />
+//       {/* <input
+//         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+//         placeholder={placeholder}
+//         onChange={(e) => {
+//           handleSearch(e.target.value);
+//         }}
+//       /> */}
+//       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+//     </div>
+//   );
+// }
+
+'use client';
+
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
 export default function Search({ placeholder }: { placeholder: string }) {
-  function handleSearch(term: string) {
-    console.log(term);
-  }
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+
+    params.set('page', '1');
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -32,14 +99,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
-      {/* <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-      /> */}
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
